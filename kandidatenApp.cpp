0cf01db5cc845ea3524984d9d912ea6ed6046409
Kevin Evans <k.evans.c@gmail.com>
2019-03-05 09:10:48 +0100

    init first file

---
+++ kandidatenApp.cpp
@@ -0,0 +1,35 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class kandidatenApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+};
+
+void kandidatenApp::setup()
+{
+}
+
+void kandidatenApp::mouseDown( MouseEvent event )
+{
+}
+
+void kandidatenApp::update()
+{
+}
+
+void kandidatenApp::draw()
+{
+	gl::clear( Color( 0, 50, 0 ) );
+	gl::drawSolidCircle(getWindowCenter(), 200);
+}
+
+CINDER_APP( kandidatenApp, RendererGl )
